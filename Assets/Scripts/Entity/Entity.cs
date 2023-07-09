d5032cc2865e5d93ecfcd31dc53454717345af21
Mr. Moo42 <GitHub@mrmoo42.com>
2023-07-08 19:10:23 -0700

    Fixed Repository

---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A generic class to represent players, enemies, items, etc.
+/// </summary>
+public class Entity : MonoBehaviour
+{
+    [SerializeField] private bool blocksMovement;
+    public bool BlocksMovement { get => blocksMovement; set => blocksMovement = value; }
+
+    public void AddToGameManager()
+    {
+        GameManager.instance.Entities.Add(this);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        transform.position += (Vector3)direction;
+    }
+}
